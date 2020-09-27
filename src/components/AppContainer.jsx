2c9332835d19d080a9ca472c68caad10e590025a
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2020-09-27 10:39:58 +0100

    my first commit

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,55 +1,53 @@
-import React, { Component } from "react";
-import {Route, Redirect, Switch } from "react-router-dom";
-import NavbarContainer from "./Navbar/NavbarContainer";
-import Login from "../pages/Login";
-import BannerSection from "../components/Banner/BannerSection";
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import NavbarContainer from './Navbar/NavbarContainer';
+import Login from '../pages/Login';
+import BannerSection from '../components/Banner/BannerSection';
 import GettingStarted from './../pages/GettingStarted';
 import Services from './../components/services/Services';
 
 class AppContainer extends Component {
+	state = {
+		pos: document.documentElement.scrollTop,
+		imglight: false,
+		navClass: ''
+	};
 
-    state = {
-        pos: document.documentElement.scrollTop,
-        imglight: false,
-        navClass: "",
-    };
+	componentDidMount() {
+		window.addEventListener('scroll', this.scrollNavigation, true);
+	}
 
-    componentDidMount() {
-        window.addEventListener("scroll", this.scrollNavigation, true);
-    }
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.scrollNavigation, true);
+	}
 
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.scrollNavigation, true);
-    }
+	scrollNavigation = () => {
+		var scrollup = document.documentElement.scrollTop;
+		if (scrollup > this.state.pos) {
+			this.setState({ navClass: 'nav-sticky', imglight: false });
+		} else {
+			this.setState({ navClass: '', imglight: false });
+		}
+	};
 
-    scrollNavigation = () => {
-        var scrollup = document.documentElement.scrollTop;
-        if (scrollup > this.state.pos) {
-            this.setState({ navClass: "nav-sticky", imglight: false });
-        } else {
-            this.setState({ navClass: "", imglight: false });
-        }
-    };
+	render() {
+		return (
+			<div>
+				{/*Importing Navbar */}
+				<NavbarContainer navClass={this.state.navClass} imglight={this.state.imglight} />
+				<Switch>
+					<Route path="/login" component={Login} />
+					<Route path="/getting-started" component={GettingStarted} />
+					<Redirect from="/" exact to="home" />
+				</Switch>
 
-    render() {
-        return (
-          <>
-                {/*Importing Navbar */}
-            <NavbarContainer navClass={this.state.navClass} imglight={this.state.imglight} />
-              <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/getting-started" component={GettingStarted} />
-                <Redirect from="/" exact to="home" />
-              </Switch>
-
-              {/*Importing Banner section */}
-              <BannerSection />
-                {/*Importing Services section */}
-              <Services />
-             
-          </>
-        );
-    }
+				{/*Importing Banner section */}
+				<BannerSection />
+				{/*Importing Services section */}
+				<Services />
+			</div>
+		);
+	}
 }
 
 export default AppContainer;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from 'reactstrap';
+//import TextField from 'material-ui/TextField';
+//import RaisedButton from 'material/RaisedButton';
 function Login() {
-    return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
-    )
+	return (
+		<div>
+			<div>
+				<Form className="text-center">
+					<section className="hero-section-2" id="">
+						<Container>
+							<Row className="justify-content-center">
+								<Col lg={6} sm={8}>
+									<div className="hero-wrapper mb-4">
+										<div className="mt-4 rounded">
+											<Link
+												to="#"
+												className="btn btn-primary mt-2 mr-2"
+												style={{ borderRadius: '40px' }}
+											>
+												C
+											</Link>
+										</div>
+										<h1 className="hero-title mb-4">Login</h1> <br />
+										<p>
+											Not a doctor?
+											<span className="text-primary">
+												<Link to="#" className="mt-2 mr-2 ">
+													Register
+												</Link>
+											</span>
+										</p>
+										<label>
+											<input type="phone" placeholder="Mobile Number" />
+											<br />
+											<br />
+
+											<input type="password" placeholder="Password" />
+											<br />
+
+											<div>
+												<input type="checkbox" />
+												<p>Remain anonymous</p>
+												<br />
+
+												<input type="checkbox" />
+												<p>
+													By signing up, I agree to{' '}
+													<Link to="#" className="text-primary mt-2 mr-2">
+														terms
+													</Link>
+												</p>
+											</div>
+											<div className="mt-4 rounded" />
+											<Button variant="contained" color="primary">
+												Sign Up
+											</Button>
+											<p>
+												Have an account?{' '}
+												<Link to="#" className="text-primary mt-2 mr-2">
+													Sign In
+												</Link>
+											</p>
+										</label>
+									</div>
+								</Col>
+							</Row>
+						</Container>
+					</section>
+				</Form>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
